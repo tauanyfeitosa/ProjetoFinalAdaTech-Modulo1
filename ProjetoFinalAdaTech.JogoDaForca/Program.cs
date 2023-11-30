2dca266d00a0848d50251b1eb01c95163e7cd1e0
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-11-30 12:46:42 -0300

    Add project files.

---
+++ ProjetoFinalAdaTech.JogoDaForca/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoFinalAdaTech.JogoDaForca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
